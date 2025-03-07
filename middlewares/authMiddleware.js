a8782c5f8ba579261e8db966d3d579ec127fedec
Laura Sdt <lauramarie.sdt@gmail.com>
2025-03-07 07:15:09 +0100

    début ajouts commentaire + suppremession lien en trop page collab

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   if (!req.session || !req.session.user) {
-    return res.redirect("/auth/login");
+    // vérif si une session est bien active et vérif si un user est stocké dans la session
+    return res.redirect("/auth/login"); // si user pas connecté, retour à la page
   }
-  next();
+  next(); // si l'user est authentifié, passage à la prochaine étape
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); // obj router fourni par Express pour gérer les routes de manière modulaire
 
+// def de la route principale
 router.get("/", (req, res) => {
+  // req.session.user : vérif si un utilisateur est stocké dans la session. Si non => auth page
   if (!req.session.user) return res.redirect("/auth/login");
   res.render("index", {
     title: "Bienvenue sur l'Intranet",
